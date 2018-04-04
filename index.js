1a6f84dcce136ce18f5c9e1ab0458751d7ed4634
Daniel Roberts <BeardOfDan@gmail.com>
2018-04-04 00:40:40 -0700

    (feat) Add react front-end

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ app.get('/', (req, res, next) => {
   res.send('Do you want to network?');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Ensures that express knows to use the 'build' of the client side code
+  app.use(express.static('client/build'));
+
+  // Ensures that React Router handles any unhandled paths
+  const path = require('path');
+  app.get('*', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
